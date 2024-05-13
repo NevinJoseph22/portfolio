9fa41fed89aca565c25e925ec5a1f8e64bba7186
NevinJoseph22 <nevinjoseph2001@gmail.com>
2024-05-13 07:11:10 +0530

    before-js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//typing animation
+
+var typed=new Typed(".typing",{
+    strings:["","Software Developer Trainee","Web Designer","Full Stack Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
